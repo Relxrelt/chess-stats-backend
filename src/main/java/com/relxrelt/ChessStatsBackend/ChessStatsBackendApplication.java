07016000458736fd93c58ada5523fbf4641fe91d
Relxrelt <mart.narva1@gmail.com>
2025-04-04 21:58:55 +0300

    Initial commit

---
+++ src/main/java/com/relxrelt/ChessStatsBackend/ChessStatsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.relxrelt.ChessStatsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessStatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessStatsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relxrelt/ChessStatsBackend/ChessStatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relxrelt.ChessStatsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessStatsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
